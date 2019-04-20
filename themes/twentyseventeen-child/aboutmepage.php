dfd3574a6ec06bf8b5ac6724fcc5dc1a4342b9a0
Susan Szymanski <susanszymanski@Susans-MacBook-Pro-3.local>
2019-04-19 22:40:38 -0400

    Created about me page

---
+++ themes/twentyseventeen-child/aboutmepage.php
@@ -0,0 +1,32 @@
+<?php /* Template Name: CustomPageT1 */ ?>
+
+<?php get_header(); ?>
+ 
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+ 
+      <?php while ( have_posts() ) : the_post(); ?>
+ 
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+ 
+            <header class="entry-header">
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+ 
+            <div class="entry-content">
+              <?php the_content(); ?>
+ 
+              <p>Hi, I’m Susan, the creator of It’s Susan’s Thing.  I’ve been called many things:  influencer, blogger, brand strategist, fit model, creative director and trend forecaster. <br> <div></div>Founded in 2019, this blog is a source of chic sophistication and style inspiration for fashionistas from all around the world. It’s Susan’s Thing is the discerning girl’s destination for all things style, travel, art and novelties in the fashion world. And probably a few other rambling (and hopefully amusing) mishaps along the way.
+				</p>
+ 
+            </div><!-- .entry-content -->
+ 
+          </article><!-- #post -->
+ 
+      <?php endwhile; // end of the loop. ?>
+ 
+    </div><!-- #content -->
+  </div><!-- #primary -->
+ 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
