7c0f036e8dad1e88d8b159723a75a4fbd347fd34
Susan Szymanski <susanszymanski@Susans-MBP-3.lan>
2019-04-19 20:02:22 -0400

    Created 404 error webpage

---
+++ themes/twentyseventeen-child/404.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php _e( 'Oh no, such a fashion faux pas!', 'twentyseventeen' ); ?></h1>
+				</header><!-- .page-header -->
+				<div class="page-content">
+					<p><?php _e( 'It looks like nothing was found here. Maybe try a different search? There is so much waiting for you :)', 'twentyseventeen' ); ?></p>
+				<img src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1537411072/180919-foreman-lfw-hero_npcr3v"/>
+					<?php get_search_form(); ?>
+
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
