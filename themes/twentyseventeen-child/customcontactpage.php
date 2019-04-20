b2e171481b9262d1a2a8e57443d82f0516c5b66c
Susan Szymanski <susanszymanski@Susans-MacBook-Pro-3.local>
2019-04-19 22:29:00 -0400

    Update customcontactpage.php

--- themes/twentyseventeen-child/customcontactpage.php
+++ themes/twentyseventeen-child/customcontactpage.php
@@ -12,7 +12,6 @@
             <header class="entry-header">
               <h1 class="entry-title"><?php the_title(); ?></h1>
             </header>
- 
             <div class="entry-content">
               <?php the_content(); ?>
  
@@ -35,14 +34,13 @@
     color: red;
   }
 </style>
- 
 <div id="respond">
   <?php echo $response; ?>
   <form action="<?php the_permalink(); ?>" method="post">
     <p><label for="name">Name: <span>*</span> <br><input type="text" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>"></label></p>
     <p><label for="message_email">Email: <span>*</span> <br><input type="text" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>"></label></p>
     <p><label for="message_text">Message: <span>*</span> <br><textarea type="text" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label></p>
-    <p><label for="message_human">Human Verification: <span>*</span> <br><input type="text" style="width: 60px;" name="message_human"> + 3 = 5</label></p>
+    <p><label for="message_human">Are you a robot in disguise? <span>*</span> <br><input type="text" style="width: 60px;" name="message_human"> + 3 = 5</label></p>
     <input type="hidden" name="submitted" value="1">
     <p><input type="submit"></p>
   </form>
