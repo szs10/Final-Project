457046d4105d85bb9d03cf4d8bc75a0637ef5705
Susan Szymanski <susanszymanski@Susans-MacBook-Pro-3.local>
2019-04-21 23:22:07 -0400

    update custom page

--- themes/twentyseventeen-child/custompage1.php
+++ themes/twentyseventeen-child/custompage1.php
@@ -1,28 +1,32 @@
 <?php /* Template Name: CustomPageT1 */ ?>
 
 <?php get_header(); ?>
-
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/page/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-
+ 
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+ 
+      <?php while ( have_posts() ) : the_post(); ?>
+ 
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+ 
+            <header class="entry-header">
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+ 
+            <div class="entry-content">
+              <?php the_content(); ?>
+ 
+              <p>Hi, I’m Susan, the creator of It’s Susan’s Thing.  I’ve been called many things:  influencer, blogger, brand strategist, fit model, creative director and trend forecaster. <br>Founded in 2019, this blog is a source of chic sophistication and style inspiration for fashionistas from all around the world. It’s Susan’s Thing is the discerning girl’s destination for all things style, travel, art and novelties in the fashion world. And probably a few other rambling (and hopefully amusing) mishaps along the way.
+				</p>
+ 
+            </div><!-- .entry-content -->
+ 
+          </article><!-- #post -->
+ 
+      <?php endwhile; // end of the loop. ?>
+ 
+    </div><!-- #content -->
+  </div><!-- #primary -->
+ 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
